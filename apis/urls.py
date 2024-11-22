473331ad8cfde4135f9262971c21880cb7949abd
Bulemu Shilukukwa <narcoa47@gmail.com>
2024-11-22 10:03:55 +0200

    full-update-urls

--- apis/urls.py
+++ apis/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, UserRegistrationView, UserLoginView
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
+# router = DefaultRouter()
+# router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('api/register/', UserRegistrationView.as_view(), name='register'),
-    path('api/login/', UserLoginView.as_view(), name='login'),
-    path('api/user/<int:pk>', UserViewSet.as_view({
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('user/<int:pk>', UserViewSet.as_view({
         'get': 'retrieve',
         'put': 'partial_update',
         'delete': 'destroy',
